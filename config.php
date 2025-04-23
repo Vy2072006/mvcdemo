535325c6b9f5700d4f6333d39ccbcc35311e6655
vancode8x <vanthq@gmail.com>
2025-04-23 08:16:58 +0700

    update config

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 return array(
-    'base' => '/PTIT24CNTT02/ss15/',
-    'baseURL' => 'http://localhost:88/PTIT24CNTT02/ss15/',
-    'assets' => '/PTIT24CNTT02/ss15/assets',
+    'base' => '/PTIT24CNTT02/mvcdemo/',
+    'baseURL' => 'http://localhost:88/PTIT24CNTT02/mvcdemo/',
+    'assets' => '/PTIT24CNTT02/mvcdemo/assets',
     'db' => array(
         'host'     => 'localhost',
         'name'     => 'ProductDB',
