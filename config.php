de65058edc1522934ec463ea36f550d20b31ee0b
vy2072006 <thaovynguyen2072006@gmail.com>
2025-04-24 14:19:01 +0700

    view gio hang - chua xong

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+    'base' => '/PTIT2402/mvcdemo/',
+    'baseURL' => 'http://localhost/PTIT2402/mvcdemo/',
+    'assets' => '/PTIT2402/mvcdemo/assets/',
+    'db' => array(
+        'host'     => 'localhost',
+        'name'     => 'ProductDB',
+        'username' => 'root',
+        'password' => ''
+    )
+);
